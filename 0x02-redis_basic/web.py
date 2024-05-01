0c7e2e2c94e2fa160d54e62cb323dfbe01e422ff
serenalilian <serenalilian99@gmail.com>
2024-05-01 19:24:57 +0000

    advanced tasks

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python3
-""" expiring web cache module """
+""" Expiring web cache module """
 
 import redis
 import requests
 from typing import Callable
 from functools import wraps
 
-redis = redis.Redis()
-
+redis_conn = redis.Redis()
 
 def wrap_requests(fn: Callable) -> Callable:
     """ Decorator wrapper """
 
     @wraps(fn)
     def wrapper(url):
-        """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
+        """ Wrapper for decorator """
+        redis_conn.incr(f"count:{url}")
+        cached_response = redis_conn.get(f"cached:{url}")
         if cached_response:
             return cached_response.decode('utf-8')
         result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
+        redis_conn.setex(f"cached:{url}", 10, result)
         return result
 
     return wrapper
 
-
 @wrap_requests
 def get_page(url: str) -> str:
-    """get page self descriptive
-    """
+    """ Get page content """
     response = requests.get(url)
     return response.text
